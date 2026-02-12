4475283fcbf16995ba21bc4e935ee11c34b9a7ea
Miguel <francomiguel79a@gmail.com>
2026-02-12 09:33:13 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// JS básico - preparado para futuras interações
+console.log("Landing Page carregada");
+
+// Aqui você pode futuramente:
+// - Adicionar menu mobile
+// - Scroll suave
+// - Integração com WhatsApp
+// - Tracking de eventos (Google Ads / GA4)
